50700cca58d2436eba5914217ffcb1955557ab01
blitzkz23 <blitzaldykazu@gmail.com>
2023-03-21 19:34:43 +0700

    feat: add initial module

---
+++ main.py
@@ -0,0 +1,8 @@
+from module.display import *
+
+def main():
+    display_menu()
+
+
+if __name__ == '__main__':
+    main()
---
+++ module/display.py
@@ -0,0 +1,3 @@
+def display_menu():
+    print('========== WELCOME TO YOUKOSO JAPARI STORE ==========')
+    print('===             SELF CASHIER SYSTEM               ===')
